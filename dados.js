dcf94681a7d89a29898a38a3b4683dd426f92183
JulioRoncato <julioroncato08@gmail.com>
2024-09-05 19:20:47 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,90 @@
+let Cartas = [
+ {   
+    titulo : "Arqueiras",
+    descricao : "As Arqueiras são uma tropa normal do Clash Royale, conhecidas por sua alta taxa de disparo e dano a longo alcance. Essa dupla de arqueiras é capaz de atacar tropas terrestres e aéreas, tornando-as versáteis em diversas situações. As Arqueiras são frequentemente usadas para defender contra tropas aéreas e terrestres, além de causar dano a construções inimigas. Sua alta taxa de disparo as torna eficazes em eliminar tropas pequenas e médias.",
+    link : "https://www.clashroyaledicas.com/2016/02/arqueiras-informacoes-wiki-tudo-sobre-a-carta.html"
+},
+{   
+    titulo : "Mago de Gelo",
+    descricao : "O Mago de Gelo é uma carta épica do Clash Royale conhecida por sua habilidade de desacelerar tropas inimigas e causar dano em área. Sua magia congelante o torna um excelente apoio para outros ataques, permitindo que suas tropas causem mais dano e que suas defesas sejam mais eficazes.Apesar de não ser um tanque, o Mago de Gelo pode ser usado tanto ofensivamente quanto defensivamente, tornando-o uma carta versátil e popular entre os jogadores. Seu dano em área o torna especialmente útil contra hordas de tropas pequenas.",
+    link : "https://www.clashroyaledicas.com/2016/03/mago-de-gelo-informacoes-estrategias-carta-lendaria-wiki.html"
+},
+{   
+    titulo : "Bárbaro de Elite",
+    descricao : "Os Bárbaros de Elite são uma tropa normal do Clash Royale, conhecidos por sua alta velocidade e dano. Essa dupla de bárbaros ferozes é capaz de causar grandes danos em um curto período de tempo. Sua habilidade especial os torna ainda mais perigosos, permitindo que eles se teleportem uma curta distância, o que os torna difíceis de prever e contra-atacar. Os Bárbaros de Elite são frequentemente usados em ataques rápidos e combinados com outras tropas para criar pressão constante sobre o adversário.",
+    link :"https://www.clashroyaledicas.com/2016/11/barbaros-de-elite-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Mago Elétrico",
+    descricao: "O Mago Elétrico é uma tropa lendária que ataca com raios elétricos, causando dano em área e atordoando as tropas inimigas. Sua alta velocidade de ataque e baixo custo de elixir o tornam uma carta versátil, podendo ser usada tanto na defesa quanto no ataque. É excelente para eliminar hordas de tropas pequenas e interromper feitiços como a Flecha. No entanto, sua baixa vida o torna vulnerável a tanques e outras tropas de alto dano.",
+    link: "https://www.clashroyaledicas.com/2016/11/mago-eletrico-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Príncipe",
+    descricao: "O Príncipe é uma tropa épica que carrega rapidamente em direção à torre inimiga, causando dano massivo ao primeiro alvo que encontrar. Seu ataque carregado o torna devastador contra tropas de alto HP, como o Gigante. No entanto, sua trajetória linear e baixa vida o tornam vulnerável a feitiços de área e tropas que podem interromper sua carga.",
+    link: "https://www.clashroyaledicas.com/2016/11/principe-informacoes-decks-estrategias-e-wiki.html"
+}, 
+{
+    titulo: "Lenhador",
+    descricao: "O Lenhador é uma tropa épica que ataca rapidamente e possui um poderoso golpe especial com seu machado, derrubando estruturas e tropas inimigas. Sua alta velocidade e dano o tornam uma ótima opção para abrir caminho para outras tropas e destruir defesas. No entanto, sua baixa vida o torna vulnerável a feitiços e tropas de longo alcance.",
+    link: "https://www.clashroyaledicas.com/2016/11/lenhador-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Bruxa Sombria",
+    descricao: "A Bruxa Sombria é uma tropa épica que invoca esqueletos constantemente e possui um ataque mágico de longo alcance. Sua capacidade de gerar um exército de esqueletos a torna uma excelente carta defensiva, capaz de distrair e eliminar tropas inimigas. Além disso, seu ataque mágico a torna eficaz contra tropas aéreas. No entanto, ela é vulnerável a feitiços de área e tropas que podem eliminá-la rapidamente.",
+    link: "https://www.clashroyaledicas.com/2016/11/bruxa-sombria-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Sparks",
+    descricao: "Os Sparks são uma dupla de dragões elétricos lendários que atacam com raios rápidos e poderosos. Sua alta velocidade de ataque e dano os tornam excelentes para eliminar tropas aéreas e terrestres de baixo HP. Além disso, seu ataque pode atordoar as tropas inimigas, interrompendo seus ataques. No entanto, eles são vulneráveis a tanques e feitiços de área.",
+    link: "https://www.clashroyaledicas.com/2016/11/sparks-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Dragão Infernal",
+    descricao: "O Dragão Infernal é uma tropa lendária que voa lentamente e causa dano em área a todas as tropas terrestres. Seu ataque incandescente aumenta de dano quanto mais tempo ele ataca o mesmo alvo, tornando-o devastador contra tanques. No entanto, sua lentidão e vulnerabilidade a tropas aéreas o tornam uma carta que requer suporte.",
+    link: "https://www.clashroyaledicas.com/2016/11/dragao-infernal-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Cemitério",
+    descricao: "O Cemitério é um feitiço épico que invoca um grande número de esqueletos em um local específico. É uma ótima opção para distrair tropas inimigas, defender contra tanques e causar dano às torres. No entanto, sua eficácia pode ser reduzida por feitiços de área como a Zap e a Flecha.",
+    link: "https://www.clashroyaledicas.com/2016/11/cemiterio-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Tronco",
+    descricao: "O Tronco é um feitiço épico que empurra as tropas inimigas para trás e causa dano em área. É excelente para eliminar tropas pequenas, interromper cargas e limpar o caminho para suas tropas. No entanto, seu alcance é limitado e ele não causa muito dano a tropas de alto HP.",
+    link: "https://www.clashroyaledicas.com/2016/11/tronco-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Mega Cavaleiro",
+    descricao: "O Mega Cavaleiro é uma tropa lendária que carrega lentamente em direção à torre inimiga, causando dano em área ao chegar ao seu destino. Sua alta vida e dano o tornam uma ameaça significativa para as torres, mas sua lentidão o torna vulnerável a feitiços de área e tropas que podem interromper sua carga.",
+    link: "https://www.clashroyaledicas.com/2016/11/mega-cavaleiro-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Lava Hound",
+    descricao: "O Lava Hound é uma tropa lendária que voa lentamente e solta pequenos lavas que causam dano em área. Quando o Lava Hound é destruído, ele se divide em pequenos lavas, que continuam causando dano. É uma excelente carta para distrair tropas inimigas e causar dano em área, mas sua lentidão o torna vulnerável a feitiços de área e tropas aéreas.",
+    link: "https://www.clashroyaledicas.com/2016/11/lava-hound-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Arqueiro Mágico",
+    descricao: "O Arqueiro Mágico é uma tropa épica que ataca com flechas mágicas, causando dano em área e atordoando as tropas inimigas. Sua alta velocidade de ataque e alcance o tornam excelente para eliminar hordas de tropas pequenas e interromper feitiços. No entanto, sua baixa vida o torna vulnerável a tanques e feitiços de área.",
+    link: "https://www.clashroyaledicas.com/2016/11/arqueiro-magico-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Bandida",
+    descricao: "A Bandida é uma tropa épica que ataca rapidamente com duas adagas, podendo atacar múltiplos alvos. Sua alta velocidade de ataque e capacidade de pular obstáculos a tornam uma ótima opção para eliminar tropas de suporte e causar dano às torres. No entanto, sua baixa vida a torna vulnerável a feitiços e tropas de longo alcance.",
+    link: "https://www.clashroyaledicas.com/2016/11/bandida-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Mineiro",
+    descricao: "O Mineiro é uma tropa épica que pode ser jogado em qualquer lugar do campo, cavando até alcançar o alvo mais próximo. É excelente para eliminar tropas de suporte, danificar tanques e atacar diretamente a torre do rei. No entanto, sua baixa vida e alcance limitado o tornam vulnerável a feitiços e tropas de longo alcance.",
+    link: "https://www.clashroyaledicas.com/2016/11/mineiro-informacoes-decks-estrategias-e-wiki.html"
+},
+{
+    titulo: "Princesa",
+    descricao: "A Princesa é uma tropa lendária que ataca de longe com flechas mágicas, causando dano em área e atordoando as tropas inimigas. Seu longo alcance e alta taxa de dano a tornam uma excelente carta defensiva e ofensiva. No entanto, sua baixa vida e lentidão a tornam vulnerável a tanques e feitiços de área.",
+    link: "https://www.clashroyaledicas.com/2016/11/princesa-informacoes-decks-estrategias-e-wiki.html"
+},
+
+];
+
+   
